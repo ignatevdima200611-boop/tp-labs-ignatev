81af18d7a4257916bf2cc0fec8183a09b28d6c49
Dima Ignatev <ignatev.dima.200611@gmail.com>
2026-09-09 16:22:04 +0700

    В консольном приложение написал выбор числа от 0 до 4, (выбор задачи и выход) а так же добавил оператор switch-case для выбора одного из вводимых значений

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Выберите число от 0 до 4, где 0 это выход, а цифры 1-4 это задания");
+
+            string ch = Console.ReadLine();
+
+            switch (ch)
+            {
+                case "1":
+                    Task1();
+                    break;
+                case "2":
+                    Task3();
+                    break;
+                case "3":
+                    Task3();
+                    break;
+                case "4":
+                    Task4();
+                    break;
+                case "0":
+                    Console.WriteLine("Выход из программы");
+                    return;
+                default:
+                    Console.WriteLine("Неккоректный ввод, выберите число от 0-4");
+                    break;
+            }
+        }
+
+    }
+}
