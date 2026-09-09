aa06de9a4addccb654cf105a654e5db17d6683cd
Dima Ignatev <ignatev.dima.200611@gmail.com>
2026-09-09 17:38:09 +0700

    В консольном приложение были исправлены ошибка при вводе цифры 2 появлялось 3 задача, исправлен case2 чтобы он перенаправлялся на статический метод Task2, а не на Task3. Добавлена 1 задача, решение факториала

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         while (true)
         {
-            Console.WriteLine("Выберите число от 0 до 4, где 0 это выход, а цифры 1-4 это задания");
+            Console.WriteLine("Выберите число от 0 до 4, где 0 это выход, а цифры 1-4 это задания: ");
 
             string ch = Console.ReadLine();
 
@@ -16,7 +16,7 @@ class Program
                     Task1();
                     break;
                 case "2":
-                    Task3();
+                    Task2();
                     break;
                 case "3":
                     Task3();
@@ -36,6 +36,24 @@ class Program
         static void Task1()
         {
             Console.WriteLine("Задание 1: ");
+
+            Console.Write("Введите n (0..20): ");
+
+            if (!int.TryParse(Console.ReadLine(), out int n) || n < 0 || n > 20)
+            {
+                Console.WriteLine("Ошибка: нужно целое число от 0 до 20.");
+                return;
+            }
+
+            Console.WriteLine($"{n}! = {Factorial(n)}");
+
+            static long Factorial(int n)
+            {
+                long result = 1;
+                for (int i = 2; i <= n; i++)
+                    result *= i;
+                return result;
+            }
 
         }
 
