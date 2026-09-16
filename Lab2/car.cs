2fec8c46b59cf9fcf407607ce3e504fa448a12c7
Dima Ignatev <ignatev.dima.200611@gmail.com>
2026-09-16 19:58:16 +0700

    создал папку Lab2 c 1 примером Car, из раздела 1 теоретического материала

---
+++ Lab2/car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Car
+{
+    private double _fuel;
+
+    public string Model { get; }
+    public double FuelConsumption { get; }
+    public double Mileage { get; private set; }
+
+    public Car(string model, double fuelConsumption)
+    {
+        if (fuelConsumption <= 0)
+            throw new ArgumentOutOfRangeException(nameof(fuelConsumption));
+        Model = model;
+        FuelConsumption = fuelConsumption;
+    }
+
+    public void Refuel(double liters)
+    {
+        if (liters <= 0) throw new ArgumentOutOfRangeException(nameof(liters));
+        _fuel += liters;
+    }
+
+    public double Drive(double distanceKm)
+    {
+        double maxDistance = _fuel / FuelConsumption * 100;
+        double actual = Math.Min(distanceKm, maxDistance);
+        Mileage += actual;
+        return actual;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var car = new Car("Lada Granta", 6.6);
+        car.Refuel(20);
+        double d = car.Drive(500);
+        Console.WriteLine($"{car.Model}: проехали {d:F0} км, пробег {car.Mileage:F0} км");
+    }
+}
